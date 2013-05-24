bb1a02b6e2f0560e050baa5509b74091a064d140
Ling Kun <lkun.erlv@gmail.com>
2013-05-24 12:03:20 +0800

    update the filereadmonitor.c which use read() to see latest update of the file

--- cpp/filereadmonitor.c
+++ cpp/filereadmonitor.c
@@ -1,7 +1,34 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
-
+typedef int FD ;
 
 int main() {
+  FD filed = open("/tmp/test_inotify", O_RDWR );
+  char buf[128];
+
+  if( !filed ) {
+    printf("Openfile error\n");
+    exit(-1);
+  }
+
+  int nbytes;
+  while(1) {
+    nbytes = read(filed, buf, 16);
+    printf("read %d bytes from file.\n", nbytes);
+    if(nbytes > 0) {
+      split_buffer_by_newline(buf); // split buffer by new line.
+    }
+    sleep(1);
+  }
+  return 0;
+}
+    
+  
+  
 
   
