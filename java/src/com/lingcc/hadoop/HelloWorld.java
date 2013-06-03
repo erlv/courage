43a559efb8a16ef284deea1ea9fad1d6049033c1
Ling Kun <lkun.erlv@gmail.com>
2013-06-03 18:46:29 +0800

    add a new code file for synchronized test, update the clrs notes

--- java/src/com/lingcc/hadoop/HelloWorld.java
+++ java/src/com/lingcc/hadoop/HelloWorld.java
@@ -1,3 +1,4 @@
+package com.lingcc.hadoop;
 
 public class HelloWorld {
   public static void main(String args[]) {
---
+++ java/src/com/lingcc/hadoop/SynchronizeTest.java
@@ -0,0 +1,39 @@
+package com.lingcc.hadoop;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+
+/**
+ * This is a class for Java Synchronized key word with function call,
+ *  switch-case statement test.
+ *  
+ *  TODO: to test what is wrong with my synchronzed code to access an synchronized
+ *  list, with one consumer, and one producer.
+ * @author erlv
+ *
+ */
+public class SynchronizeTest {
+	  public   List<String> readyMapSpillFiles 
+  		= Collections.synchronizedList(new ArrayList<String>());
+	  
+	  
+	  public boolean consumer() {
+		return false;
+		  
+	  }
+	  
+	  
+	  
+	  private class Producer extends Thread {
+		  public Producer() {
+			  setName("Thread for producer");
+			  setDaemon(true);
+			  
+		  }
+	  }
+	  
+	  
+}
