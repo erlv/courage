8c9b4f0d5fa796276502d0ae7758b66c54f3b5e2
Ling Kun <lkun.erlv@gmail.com>
2013-08-08 10:24:53 +0800

    new changes

---
+++ cpp/src/SO_c11Feature.cc
@@ -0,0 +1,8 @@
+#include <tr1/unordered_set> // no warnings
+using namespace std;
+
+int main()
+{
+    tr1::unordered_set<int> s;
+    return 0;
+}
---
+++ py-script/dirCompare.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+## This script is used for compare files in two directories
+##  1. Whether they have the same files
+##  2. Whether there file contents are the same (using md5checksum)
+
+
+
+## 1. Found out all files in the given two directory
+## 2. use md5sum to generate the checksum of each file 
+## 3. compare the filename and the checksum of the two directory
+
+
+
